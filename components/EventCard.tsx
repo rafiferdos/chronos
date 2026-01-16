157fca13e6597fee8f7a3632416fd2f7b8395925
Rafi Ferdos <rafiferdos@gmail.com>
2026-01-16 09:53:30 +0600

    add icons to EventCard component for improved visual representation of event types; enhance layout with a header row for better organization

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils"
+import { Briefcase, HomeIcon } from "lucide-react-native"
 import { View } from "react-native"
 
 interface EventCardProps {
@@ -17,6 +18,25 @@ export function EventCard({
   return (
     <View className={cn("mb-4 p-4 rounded-2xl border-l-4 shadow-sm", isPink ? 'bg-[#FFF0F0] border-[#FF8A8A]' : "bg-[#F5F3FF] border-[#A78BFA]")}>
       
+      {/* header row */}
+      <View className="flex-row justify-between items-start mb-4">
+        <View className="flex-row items-center gap-3">
+
+          {/* icon circle */}
+          <View className="h-10 w-10 rounded-full items-center justify-center bg-white/60">
+            {
+              icon === 'work' ? (
+                <Briefcase size={18} color={isPink ? '#9B2C2C' : '#5B21B6'} />
+              ) : (
+                  <HomeIcon size={18} color={isPink ? '#9B2C2C' : '#5B21B6'} />
+              )
+            }
+
+          </View>
+
+        </View>
+
+      </View>
     </View>
   )
 }
